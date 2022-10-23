8a916f5dc91bef052a45b83147aa92ce616151c2
Touseef Ahmad <touseefahmad@A006-00718.local>
2022-10-23 13:54:13 +0500

    :zap: Added few example routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', (req, res) => {
+  res.send('Got a post request');
+})
 
 module.exports = router;
